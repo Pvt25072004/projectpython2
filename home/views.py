c51da1c32248d4a34be00051083e872d55ae2562
tienpham <phamtienlq955@gmail.com>
2024-01-16 23:29:13 +0700

    add folder homepage

--- home/views.py
+++ home/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
-def index(request):
-    response = HttpResponse()
-    response.writelines('<h1>Welcome to My Project</h1>')
-    response.write('<p>This is my app</p>')
-    return response
