8a6bd8e3d3523f5c88bfa6140e7a93c9f00d2e14
tienpham <phamtienlq955@gmail.com>
2024-01-19 19:35:36 +0700

    finish homepage

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.home),
+    path('product', views.product),
+    path('faq', views.faq),
+    path('blog', views.blog),
+    path('about', views.about),
+    path('login', views.login),
+    path('signup', views.signup),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-    template_index = ('index.html')
-    return render(request, template_index)
+def home(request):
+    template_home = ('home.html')
+    return render(request, template_home)
+def product(request):
+    template_product = ('product.html')
+    return render(request, template_product)
+def faq(request):
+    template_faq = ('faq.html')
+    return render(request, template_faq)
+def blog(request):
+    template_blog = ('blog.html')
+    return render(request, template_blog)
+def about(request):
+    template_about = ('about.html')
+    return render(request, template_about)
+def login(request):
+    template_login = ('login.html')
+    return render(request, template_login)
+def signup(request):
+    template_signup = ('signup.html')
+    return render(request, template_signup)
