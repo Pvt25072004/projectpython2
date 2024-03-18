9cb32dfdebbdff6c37fa05522b0a4ffc57b079ca
tienpham <phamtienlq955@gmail.com>
2024-03-18 16:23:18 +0700

    update March 18

--- home/views.py
+++ home/views.py
@@ -2,23 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    template_home = ('home.html')
+    template_home = ('page/home.html')
     return render(request, template_home)
 def product(request):
-    template_product = ('product.html')
+    template_product = ('page/product.html')
     return render(request, template_product)
 def faq(request):
-    template_faq = ('faq.html')
+    template_faq = ('page/faq.html')
     return render(request, template_faq)
 def blog(request):
-    template_blog = ('blog.html')
+    template_blog = ('page/blog.html')
     return render(request, template_blog)
 def about(request):
-    template_about = ('about.html')
+    template_about = ('page/about.html')
     return render(request, template_about)
 def login(request):
-    template_login = ('login.html')
+    template_login = ('page/login.html')
     return render(request, template_login)
 def signup(request):
-    template_signup = ('signup.html')
+    template_signup = ('page/signup.html')
     return render(request, template_signup)
